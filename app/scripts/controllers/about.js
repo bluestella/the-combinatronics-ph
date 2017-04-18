c0fba81b5667f701a7d14c390eceae339dd48d6c
bluestella <joseph.buluran@gmail.com>
2017-04-18 09:44:35 +0800

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name theCombinatronicsPhApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the theCombinatronicsPhApp
+ */
+angular.module('theCombinatronicsPhApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name theCombinatronicsPhApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the theCombinatronicsPhApp
+ */
+angular.module('theCombinatronicsPhApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
